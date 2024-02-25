a06cca7e21d2da43cdf4f958acdaf97fcbb158a7
12giorgi12 <giorgi.wakadze.08@mail.ru>
2024-02-25 19:13:22 +0400

    second commit

---
+++ day2/goa homework2.py
@@ -0,0 +1,2 @@
+print("gavicani da davumegobrdi: demetre eksulidzes")
+print("me vfiqrob rom dzalian kargi gadawyvetileba mivige rom shemovedi goashi da vfiqrob rom bevr megobrebs gavicen")
---
+++ day4/homework4.py
@@ -0,0 +1,10 @@
+print("pleas enter your name")
+name=input()
+ 
+print("user input is:  " + name)
+
+print("enter your surname")
+
+surname=input()
+
+print("user input is:  " + surname)
