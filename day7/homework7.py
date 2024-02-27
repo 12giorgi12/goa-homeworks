979a03407124489483cb2c1375cae5f48f89cde9
12giorgi12 <giorgi.wakadze.08@mail.ru>
2024-02-27 23:35:19 +0400

    second commit

---
+++ day7/homework7.py
@@ -0,0 +1,23 @@
+for i in range(0,15,2):
+     
+     print( i,)
+
+""
+
+for i in range(0,21):
+
+    print(i)
+
+""
+
+for i in range(0,6):
+
+    print(i)
+
+    sum =0
+
+for i in range(0,6):
+
+     sum += i
+
+     print(sum)
