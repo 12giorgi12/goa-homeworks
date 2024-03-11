6b98949e05d0ff7fdc557d405c274ce1b5dac263
12giorgi12 <giorgi.wakadze.08@mail.ru>
2024-03-11 19:16:36 +0400

    second commit

---
+++ day11/homework11.py
@@ -0,0 +1,29 @@
+# list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in list:
+#     print(list[i] * 2)
+
+# for i in list:
+#     print(list[i] / 2)
+
+# for i in list:
+#     print(list[i] + 2)
+
+# for i in list:
+#     print(list[i] - 2)
+
+
+list2 = ["home", "car" ,"bike", "dog"]
+
+print(list2[0])
+
+print(list2[1])
+
+print(list2[2])
+
+print(list2[3])
+
+
+
+# products = ['lays', 'coca-cola', 'fanta', 'sprite']
+# choice = int(input('Enter a num: '))
+# print(products[choice])
